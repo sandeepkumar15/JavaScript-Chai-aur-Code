1d31766415787c7deab794b63add777ad697996d
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-19 17:41:17 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all js code as new version
+
+//  alert(3 + 3) //we are using nodeJs, not browser
+
+console.log(3+3);
+ 
+console.log("Sandeep");  //code readability should be high
+
+let name = "Sandeep"
+let age = 18
+let isLoggedIn = true
+
+// Primitive Datatypes
+//number => 2 to power 53
+// BiogInt
+//string => ""
+//boolean =>true/false
+//null => standalone value or Type Object
+//undefined => value not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof(undefined)); //undefined
+console.log(typeof(null)); //object
