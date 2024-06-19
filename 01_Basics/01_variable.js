e0ffee02cc2fadae2adfb9f11c9263c4630ffd60
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-19 17:41:45 +0000

    var

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sandeep@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+accountEmail = "tanya@sandeep.com"
+accountPassword = "151208"
+accountCity = "Bemgaluru"
+
+let accountState;
+
+// accountId = 2  //Not allowed
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
