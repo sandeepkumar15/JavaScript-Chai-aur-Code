c8d870eec05560cfb80f4d0f5be41b5b4d5f7cdf
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-20 09:02:11 +0000

    Comparision Data types done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,26 @@
+//***********************Comparision**********************
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// DataType Must be Same
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.logh(null == 0); 
+// console.logh(null >= 0); 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Strict Check // ===
+
+console.log("2" === 2);
