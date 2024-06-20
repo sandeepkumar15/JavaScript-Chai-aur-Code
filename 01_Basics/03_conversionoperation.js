79c27286226bd7c4e6f7894fbe97e8ab1cd2e871
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-20 08:59:20 +0000

    Comparision operator complete

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -19,15 +19,73 @@
 // let isLoggedIn = 1;
 // let isLoggedIn = "";
 // let isLoggedIn = " ";
-let isLoggedIn = "Sandeep";
+// let isLoggedIn = "Sandeep";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false; " " => true
 //"sandeep => true"
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//******************************Operations*****************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello";
+// let str2 = "sandeep";
+
+// let str3 = str1 +  str2;
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 =num3 =2+2;
+// console.log(num3);
+
+//***********************Comparision**********************
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// DataType Must be Same
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.logh(null == 0); 
+// console.logh(null >= 0); 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Strict Check // ===
+
+console.log("2" === 2);
