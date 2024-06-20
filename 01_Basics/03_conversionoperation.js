2a52b842a64b4d661c249ea8c2043a0103e92158
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-20 09:01:47 +0000

    conversion operation done

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -63,29 +63,3 @@
 // num1 = num2 =num3 =2+2;
 // console.log(num3);
 
-//***********************Comparision**********************
-
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 <= 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
-
-
-// DataType Must be Same
-
-// console.log("2" > 1);
-// console.log("02" > 1);
-
-// console.log(null > 0);
-// console.logh(null == 0); 
-// console.logh(null >= 0); 
-
-// console.log(undefined == 0);
-// console.log(undefined > 0);
-// console.log(undefined < 0);
-
-// Strict Check // ===
-
-console.log("2" === 2);
