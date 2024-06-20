ecbbfef2e6261f8c042c8367c9009199689bad0b
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-20 19:45:37 +0000

    Learn About String and String Declaration

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,21 @@
+// const name = "Sandeep";
+// const repoCount = 50;
+
+// console.log(name + repoCount + "  value");  //concatination
+//Old Style of concatination
+
+// console.log(`Hello My name is ${name} and my repo com=unt is ${repoCount}`);
+
+// console.log(`${name}  ${repoCount}`);
+
+//String Declaration method-2
+
+// let gameName = "Sandeep";
+
+const gameName = new String('Sandeep');
+
+console.log(gameName);
+// console.log(gameName[6]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+console.log(gameName.substring(0,4));
