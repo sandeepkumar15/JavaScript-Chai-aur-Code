2068812169dc2e088cc60e7496e069839d2eea82
SANDEEP KUMAR <sandeepdps2@gmail.com>
2024-06-20 06:02:55 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionoperation.js
@@ -0,0 +1,33 @@
+// let score = 33;
+// let score = "33abc";
+// let score = null;
+// let score = true;
+// let score = false;
+// let score = "sandeep";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN (Not a Number)
+//true => 1; false => 0;
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+// let isLoggedIn = " ";
+let isLoggedIn = "Sandeep";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false; " " => true
+//"sandeep => true"
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
